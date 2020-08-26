f8d681a78c40ba1551665d6f660f69ad273d0139
Timothy Do <timothydobsa@gmail.com>
2020-08-26 09:19:13 -0700

    migrated css and js to seperate files

---
+++ main.js
@@ -0,0 +1,12 @@
+        //for switching energy modes
+        (function(){
+            const t = document.querySelector('#toggle-theme');
+            let light;
+            t.onclick = () => {
+                document.querySelector('html').toggleAttribute('light');
+                light = !light;
+                t.innerText = light ? '🌙' : '☀️';
+                localStorage.setItem('light-theme', light);
+            }
+            if (JSON.parse(localStorage.getItem('light-theme', light))) t.click();
+        }());
